f1a0667d111eb0b9827a8941dfb2a21a3923c361
Pedro Sanchez Naranjo <p.sanchez.naranjo@outlook.com>
2020-05-14 17:53:43 +0200

    Azure Redis Cache Solution Sample

---
+++ AzureRedisCache/AzureRedisCache.Repository.Contracts/IWeatherForecastRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AzureRedisCache.Repository.Contracts
+{
+    public interface IWeatherForecastRepository
+    {
+        IEnumerable<WeatherForecast> GetForecast();
+    }
+}
---
+++ AzureRedisCache/AzureRedisCache/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using AzureRedisCache.CommonServices.Contracts;
+using AzureRedisCache.Repository.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace AzureRedisCache.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherForecastRepository _weatherForecastRepository;
+        private readonly ICacheService _cacheService;
+
+        public WeatherForecastController(IWeatherForecastRepository weatherForecastRepository, 
+                                         ICacheService cacheService,
+                                         ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _weatherForecastRepository = weatherForecastRepository ?? throw new ArgumentNullException(nameof(weatherForecastRepository));
+            _cacheService = cacheService ?? throw new ArgumentNullException(nameof(cacheService));
+        }
+
+        [HttpGet("Get")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return _weatherForecastRepository.GetForecast();
+        }
+
+        [HttpGet("GetCached")]
+        public IEnumerable<WeatherForecast> GetCached()
+        {
+            return _cacheService.GetUsingCache("TestCache", () => _weatherForecastRepository.GetForecast());
+        }
+    }
+}
